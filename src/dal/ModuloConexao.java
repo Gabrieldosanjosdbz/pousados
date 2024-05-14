d601ef8b7ee58f6e6a5ed22fb68d8b26bcb8824c
gabriel <gabrieldosanjosdbz@gmil.com>
2024-05-14 11:58:05 -0300

    second commit

--- src/dal/ModuloConexao.java
+++ src/dal/ModuloConexao.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
-/**
- *
- * @author CT Desenvolvimento
- */
+import java.sql.*;
+
 public class ModuloConexao {
     
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3307/pousados";
+        String user = "root";
+        String password = "";
+        
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        }
+        catch (Exception e) {
+            return null;
+        }
+        
+    }
 }
